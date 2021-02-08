20d48904e8821b36c4ed3074576d1a3c960b453c
Landon Wills <landonjwdev@gmail.com>
2021-02-07 20:23:03 -0500

    test push

--- stress_test_client.py
+++ stress_test_client.py
@@ -172,4 +172,4 @@ while True:
         stop_stress_test()
     else:
         # If there isn't a stress test running, sleep for a second to save resources.
-        time.sleep(6)
+        time.sleep(7)
