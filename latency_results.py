5d9ea16e8dc537029b328840ceffc41c4b722b13
Landon Wills <landonjwdev@gmail.com>
2021-03-06 18:33:30 -0500

    added total latency result functions

--- latency_results.py
+++ latency_results.py
@@ -45,3 +45,31 @@ class LatencyResultsProcessor:
             if latency == None:
                 lost_packets = lost_packets + 1
         return lost_packets
+
+def get_total_average_latency(results_processors):
+    sum = 0
+    for processor in results_processors:
+        sum = sum + processor.get_average_latency()
+    return sum / len(results_processors)
+
+def get_total_max_latency(results_processors):
+    max = -1
+    for processor in results_processors:
+        processor_max = processor.get_max_latency()
+        if processor_max > max:
+            max = processor_max
+    return max
+
+def get_total_min_latency(results_processors):
+    min = -1
+    for processor in results_processors:
+        processor_min = processor.get_min_latency()
+        if processor_min < min or min == -1:
+            min = processor_min
+    return min
+
+def get_total_packets_lost(results_processors):
+    sum = 0
+    for processor in results_processors:
+        sum = sum + processor.get_packets_lost()
+    return sum
