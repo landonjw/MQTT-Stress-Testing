4d6296aee094ac43552c3b3840e59dd6e806cf26
landonjw <landon.wills@mohawkcollege.ca>
2021-04-12 22:34:18 -0400

    documented latency results

--- latency_results.py
+++ latency_results.py
@@ -1,39 +1,110 @@
 class LatencyResultsProcessor:
+    """
+    Stores and calcualtes latency information from messages published by stress test clients.
+    A processor instance should only be used by a single stress test client. 
+
+    Attributes
+    ----------
+    total_packets: Integer
+        The total number of packets that a stress test client is responsible for publishing.
+
+    Methods
+    -------
+    add_latency_info(packet_num: Integer, latency: Integer)
+        Adds the latency information for a given packet
+
+    add_offset(packet_num: Integer, offset: Integer)
+        Offsets the latency of a packet by the given offset value.
+        This is used to ignore the duration it takes to publish the data on the client.
+
+    get_average_latency(): Float
+        Gets the average latency of packets received by the client in milliseconds.
+        Running this before the client has published all packets may give inaccurate results.
+
+    get_min_latency(): Integer
+        Gets the minimum latency of packets received by the client in milliseconds.
+        Running this before the client has published all packets may give inaccurate results.
+
+    get_max_latency(): Integer
+        Gets the maximum latency of packets received by the client in milliseconds.
+        Running this before the client has published all packets may give inaccurate results.
+
+    get_packets_lost(): Integer
+        Gets the number of packets that never had their latency registered.
+        Running this before the client has published all packets may give inaccurate results.
+    """
+
     def __init__(self, total_packets):
-        self.packet_data = []
-        self.offset = []
+        self._packet_data = [None] * total_packets
+        self._offset = [None] * total_packets
         self.total_packets = total_packets
-        for i in range(0, total_packets):
-            self.packet_data.append(None)
-            self.offset.append(None)
         
     def add_latency_info(self, packet_num, latency):
-        if self.offset[packet_num] != None:
-            self.packet_data[packet_num] = max(latency - self.offset[packet_num], 1)
+        """
+        Adds latency information about a given packet.
+
+        Arguments
+        ---------
+        packet_num: Integer
+            The ID of the packet to add latency information for
+        latency: Integer
+            The latency in milliseconds
+        """
+        if self._offset[packet_num] != None:
+            self._packet_data[packet_num] = max(latency - self._offset[packet_num], 1)
         else:
-            self.packet_data[packet_num] = latency
+            self._packet_data[packet_num] = latency
 
     def add_offset(self, packet_num, offset):
-        if self.packet_data[packet_num] != None:
-            self.packet_data[packet_num] = max(self.packet_data[packet_num] - offset, 1)
+        """
+        Adds an offset for a given packet.
+        This is used to deduct the time it takes to publish a message on the client from the latency.
+
+        Arguments
+        ---------
+        packet_num: Integer
+            The ID of the packet to add offset for
+        offset: Integer
+            The offset in milliseconds
+        """
+        if self._packet_data[packet_num] != None:
+            self._packet_data[packet_num] = max(self._packet_data[packet_num] - offset, 1)
         else:
-            self.offset[packet_num] = offset
+            self._offset[packet_num] = offset
     
     def get_average_latency(self):
+        """
+        Gets the average latency in milliseconds of the packets a client has published
+
+        Returns
+        -------
+        Float
+            The average latency in milliseconds of all packets published by the client
+        """
+        # Get sum of latency 
         total_latency = 0
-        for latency in self.packet_data:
+        for latency in self._packet_data:
             if latency != None:
                 total_latency = total_latency + latency
         packets_lost = self.get_packets_lost()
+        # Handle the case where all packets are lost to prevent division by zero
         if packets_lost == self.total_packets:
             return -1
         else:
             return total_latency / self.total_packets - self.get_packets_lost()
 
     def get_max_latency(self):
+        """
+        Gets the maximum latency in milliseconds of the packets a client has published
+
+        Returns
+        -------
+        Integer
+            The maximum latency in milliseconds of all packets published by the client
+        """
         max_latency = -1
-        for latency in self.packet_data:
-            if latency == None:
+        for latency in self._packet_data:
+            if latency == None: # Handle lost packets
                 continue
             else:
                 if latency > max_latency:
@@ -41,9 +112,17 @@ class LatencyResultsProcessor:
         return max_latency
 
     def get_min_latency(self):
+        """
+        Gets the minimum latency in milliseconds of the packets a client has published
+
+        Returns
+        -------
+        Integer
+            The minimum latency in milliseconds of all packets published by the client
+        """
         min_latency = -1
-        for latency in self.packet_data:
-            if latency == None:
+        for latency in self._packet_data:
+            if latency == None: # Handle lost packets
                 continue
             else:
                 if latency < min_latency or min_latency == -1:
@@ -51,19 +130,53 @@ class LatencyResultsProcessor:
         return min_latency
     
     def get_packets_lost(self):
+        """
+        Gets the number of packets that were never received from the client
+
+        Returns
+        -------
+        Integer
+            The number of packets that were never received
+        """
         lost_packets = 0
-        for latency in self.packet_data:
+        for latency in self._packet_data:
             if latency == None:
                 lost_packets = lost_packets + 1
         return lost_packets
 
 def get_total_average_latency(results_processors):
+    """
+    Gets the average latency of all results
+
+    Arguments
+    ---------
+    results_processors: List of LatencyResultsProcessor
+        A list of results to get average latency from
+
+    Returns
+    -------
+    Float
+        The average latency from all latency results
+    """
     sum = 0
     for processor in results_processors:
         sum = sum + processor.get_average_latency()
     return sum / len(results_processors)
 
 def get_total_max_latency(results_processors):
+    """
+    Gets the maximum latency of all results
+
+    Arguments
+    ---------
+    results_processors: List of LatencyResultsProcessor
+        A list of results to get maximum latency from
+
+    Returns
+    -------
+    Integer
+        The maximum latency of all latency results
+    """
     max = -1
     for processor in results_processors:
         processor_max = processor.get_max_latency()
@@ -72,6 +185,19 @@ def get_total_max_latency(results_processors):
     return max
 
 def get_total_min_latency(results_processors):
+    """
+    Gets the minimum latency of all results
+
+    Arguments
+    ---------
+    results_processors: List of LatencyResultsProcessor
+        A list of results to get minumum latency from
+
+    Returns
+    -------
+    Integer
+        The minimum latency of all latency results
+    """
     min = -1
     for processor in results_processors:
         processor_min = processor.get_min_latency()
@@ -80,6 +206,19 @@ def get_total_min_latency(results_processors):
     return min
 
 def get_total_packets_lost(results_processors):
+    """
+    Gets the total packets lost from all results
+
+    Arguments
+    ---------
+    results_processors: List of LatencyResultsProcessor
+        A list of results to get total packets lost from
+
+    Returns
+    -------
+    Integer
+        The number of packets lost from all results processors
+    """
     sum = 0
     for processor in results_processors:
         sum = sum + processor.get_packets_lost()
